ef0715513fd8b13942c9101704d7685e563b13a5
Hubert Getrouw <>
2018-09-03 17:00:50 +0200

    presale
    presale

--- contracts/WeatherCrowdsale.sol
+++ contracts/WeatherCrowdsale.sol
@@ -2,6 +2,7 @@ pragma solidity 0.4.24;
 
 import "openzeppelin-solidity/contracts/crowdsale/Crowdsale.sol";
 import "openzeppelin-solidity/contracts/crowdsale/validation/TimedCrowdsale.sol";
+import "openzeppelin-solidity/contracts/crowdsale/price/IncreasingPriceCrowdsale.sol";
 import "openzeppelin-solidity/contracts/crowdsale/validation/CappedCrowdsale.sol";
 import "openzeppelin-solidity/contracts/crowdsale/emission/MintedCrowdsale.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
@@ -10,7 +11,7 @@ interface MintableERC20 {
     function finishMinting() public returns (bool);
 }
 
-contract WeatherCrowdsale is Crowdsale, TimedCrowdsale, CappedCrowdsale, MintedCrowdsale {
+contract WeatherCrowdsale is Crowdsale, TimedCrowdsale, IncreasingPriceCrowdsale, CappedCrowdsale, MintedCrowdsale {
     bool public isFinalized = false;
 
     event Finalized();
@@ -21,15 +22,19 @@ contract WeatherCrowdsale is Crowdsale, TimedCrowdsale, CappedCrowdsale, MintedC
         ERC20 _token,
         uint256 _openingTime,
         uint256 _closingTime,
-        uint256 _cap
+        uint256 _cap,
+        uint256 _initialRate, 
+        uint256 _finalRate
     ) 
         Crowdsale(_rate, _wallet, _token)
         TimedCrowdsale(_openingTime, _closingTime)
+        IncreasingPriceCrowdsale(_initialRate, _finalRate)
         CappedCrowdsale(_cap)
         public
     {
 
     }
+
 
     function finalize() public {
         require(!isFinalized);
